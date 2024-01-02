5c7f5744b5801895a4304c76325b0db4440f293c
Gustavo Lobo <122462442+Gustavolcl@users.noreply.github.com>
2024-01-02 17:47:53 -0300

    Delete  directory

--- Food Explorer Front/src/components/ButtonText/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.button`
-    background: none;
-    border: none;
-    font-size: 16px;
-    color: ${({theme}) => theme.COLORS.GRAY};
-    width: 100%;
-`;
--- Food Explorer Front/src/components/Textarea/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from './styles';
-
-export function Textarea( { title, value, ...rest }){
-    return(
-        <Container {...rest}>
-            {value}
-        </Container>
-    );
-}
